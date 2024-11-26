addbdf540913dc22bd02bb39c9aa18a0f1633f0e
Manish Dhanani <manish.dhanani.91@gmail.com>
2024-11-26 10:06:36 +0530

    Set up the Project Structure and Dependencies

---
+++ app/build.gradle.kts
@@ -0,0 +1,70 @@
+plugins {
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.jetbrains.kotlin.android)
+    alias(libs.plugins.jetbrains.kotlin.kapt)
+    alias(libs.plugins.dagger.hilt.android)
+}
+
+android {
+    namespace = "com.task.pixabay"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "com.task.pixabay"
+        minSdk = 24
+        targetSdk = 35
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+
+    // Enable data binding
+    viewBinding {
+        enable = true
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+    kotlinOptions {
+        jvmTarget = JavaVersion.VERSION_11.toString()
+    }
+}
+
+dependencies {
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.androidx.activity)
+    implementation(libs.androidx.constraintlayout)
+    implementation(libs.material)
+
+    // Hilt - Dependency Injection
+    implementation(libs.hilt.android)
+    kapt(libs.hilt.compiler)
+
+    // Coroutines for async tasks
+    implementation(libs.coroutines.android)
+
+    // Lifecycle components (LiveData, ViewModel)
+    implementation(libs.lifecycle.livedata)
+    implementation(libs.lifecycle.viewmodel)
+
+    // Data Binding for XML and UI binding
+    implementation(libs.databinding.runtime)
+
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+}
---
+++ app/src/main/java/com/task/pixabay/PixabayApp.kt
@@ -0,0 +1,10 @@
+package com.task.pixabay
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+/**
+ * Enable Hilt for Dependency Injection
+ */
+@HiltAndroidApp
+class PixabayApp : Application()
